54ba3833d8ecf48af8518afcb27b08bbc51e22b8
Rafael Bautista <323bautista@gmail.com>
2017-12-02 21:10:34 -0800

    added webpack, babel and react base

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class Hello extends Component {
+    render() {
+        return (
+                <div>
+                    Hello future empower
+                </div>
+               );
+    }
+}
+render(<Hello />, document.getElementById('app'));
